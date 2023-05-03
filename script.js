dbac329359a2a5a3ef7a759dc2c5a675a5451644
Vaibhav Humbade <117819312+vaibhav7174@users.noreply.github.com>
2023-05-03 10:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener('input', () => {
+	let str = evaluatedText.value;
+	letterCount.innerHTML = `${str.length}`;
+});
